7f9897dbd192baa4869e832fee40111694826186
Danil <danila_monster@mail,ru>
2022-08-29 20:51:52 +0300

    Task55

---
+++ Task002/Program.cs
@@ -0,0 +1,54 @@
+﻿//  Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+//  В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+
+int [,] CreateMatrix(int rowCount, int columnCount, int min, int max)     
+{
+    int[,] array = new int[rowCount, columnCount];
+    for(int row = 0; row < rowCount; ++row)                  
+    {
+        for(int column = 0; column < columnCount; ++column)
+        {
+            array[row, column] = new Random().Next(min,max + 1);  
+        }
+    }
+    return array;
+}
+
+void PrintMatrix(int[,] array)
+
+{
+    for(int row = 0; row < array.GetLength(0); ++row)                  
+    {
+        for(int column = 0; column < array.GetLength(1); ++column)
+        {
+            Console.Write($"{array[row, column]}\t");
+        }
+        Console.WriteLine();
+    }    
+           
+}
+
+
+int[,] ReversMatrix (int[,] array)
+{
+    int [,] matrix = new int[array.GetLength(1),array.GetLength(0)];
+    
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            matrix[j, i] = array[i, j];
+        }
+       
+    }
+
+    return matrix;
+}
+
+
+int[,] matrix = CreateMatrix(3, 3, 0, 99);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[,] reverMatrix = ReversMatrix(matrix);
+PrintMatrix(reverMatrix);
